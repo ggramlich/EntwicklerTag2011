5b61445664e92181cf5c183bd2363bb4cb004ac4
Gregor Gramlich <gramlich@eosc.de>
2011-05-26 16:38:09 +0200

    Angefangen, Cartoon Bilder in Präsentation zu integrieren

--- FitNesseRoot/files/jquery-presentation/scripts/init.js
+++ FitNesseRoot/files/jquery-presentation/scripts/init.js
@@ -1,14 +1,44 @@
 $(document).ready(function () {
 	$('body').addClass('andrena');
+	resizeCartoonImages();
 });
 
 if ('undefined' !== typeof showPresentation && showPresentation) {
 	$(document).ready(function () {
 		var cssFiles = ['/files/css/fitnesse.css', '/files/jquery-presentation/stylesheets/presentation.css'];
-		var switcher = new PresentationModeSwitcher(cssFiles); // that is F9, ESC
-		switcher.bindToKey(120, 27);
+		var switcher = new PresentationModeSwitcher(cssFiles);
+		switcher.bindToKey(120, 27); // that is F9, ESC
 		switcher.toggle();
 	});
+}
+
+function resizeCartoonImages() {
+	$('img.cartoon').each(function() {
+		var cartoon = $(this).get(0);
+		$cartoon = $(cartoon);
+		var size = $cartoon.data('size');
+		if (size) {
+			resizeCartoon($(cartoon), size)
+		} else {
+			// http://stackoverflow.com/questions/318630/get-real-image-width-and-height-with-javascript-in-safari-chrome
+			$("<img/>").attr("src", $(cartoon).attr("src")).load(function() {
+				size = {width: this.width, height: this.height};
+				resizeCartoon($(cartoon), size)
+				$cartoon.data('size', size);
+			});
+		}
+	});
+}
+
+function resizeCartoon($cartoon, size) {
+	var width = size.width;
+	var height = size.height;
+	var viewportwidth = $(window).width();
+	var viewportheight = $(window).height();
+	var factor = 0.8 * Math.min(viewportwidth / width, viewportheight / height * 0.9);
+	$cartoon.width(factor * width);
+	$cartoon.height(factor * height);
+	console.log($cartoon + " " + $cartoon.width() + "x" + $cartoon.height());
 }
 
 function PresentationModeSwitcher(cssFiles) {
@@ -40,6 +70,7 @@ function PresentationModeSwitcher(cssFiles) {
 			$('#slides').children('.slide').show();
 			loadStyleSheet(cssFiles[0]);
 		}
+		resizeCartoonImages();
 	};
 	
 	this.bindToKey = function (key, escapeKey) {
