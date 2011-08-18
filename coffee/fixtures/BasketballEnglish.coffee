45de0f0c4daa19ccf08bd6a70702345c902c26b1
Gregor Gramlich <gramlich@eosc.de>
2011-08-18 22:48:59 +0200

    Convert tests to english

---
+++ coffee/fixtures/BasketballEnglish.coffee
@@ -0,0 +1,82 @@
+loadFile = (filename) ->
+  JsSlim.loadJsFile(filename + '.js')
+
+loadFile('BbScoutModel');
+
+model = this.BbScout.model
+
+class Team
+  constructor: (@name) ->
+    @team = new model.Team(@name)
+    model.teams[@name] = @team
+
+  setNumber: (@number) ->
+  setFirstName: (@firstName) ->
+  setLastName: (@lastName) ->
+
+  execute: ->
+    player = new model.Player(@number, @firstName, @lastName);
+    @team.addPlayer player
+
+class SelectPlayer
+  constructor: (@teamname, @number) ->
+    @player = model.teams[@teamname]?.getPlayer(@number)
+
+  playerName: -> @player?.name() ? 'unknown'
+
+POINT_TYPES =
+  'free throw': 'Freethrow'
+  'free throws': 'Freethrow'
+  'field goal': 'Fieldgoal'
+  'field goals': 'Fieldgoal'
+  'three pointer': 'Threepointer'
+  'three pointers': 'Threepointer'
+
+normalizePointType = (pointType) -> POINT_TYPES[pointType]
+
+
+PlayerSimulation = (->
+  NUMBER = '1'
+
+  class
+    constructor: -> @player = new model.Player(NUMBER)
+
+    points: -> @player.points
+
+    setPoints: (points) -> @player.points = parseInt points
+
+    scores: (pointType) -> @player.scores normalizePointType(pointType)
+
+    misses: (pointType) -> @player.misses normalizePointType(pointType)
+
+    scored: (pointType) -> @player.scored normalizePointType(pointType)
+
+    attempted: (pointType) -> @player.attempted normalizePointType(pointType)
+)()
+
+class GameSimulation
+  constructor: (@teamNameA, @teamNameB) ->
+    @teamA = @getTeam(@teamNameA)
+    @teamB = @getTeam(@teamNameB)
+
+  score: -> @teamA?.points() + ':' + @teamB?.points()
+  
+  playerOfScoresA: (number, teamName, pointType) ->
+    player = @getPlayer(teamName, number)
+    pointTypeOk = player?.scores normalizePointType(pointType)
+    player? and pointTypeOk
+  
+  getPlayer: (teamName, number) -> @getTeam(teamName)?.getPlayer(number)
+  
+  getTeam: (teamName) -> model.teams[teamName]
+
+
+# Export the fixtures as module BasketballEnglish
+
+this.BasketballEnglish =
+  Team: Team
+  SelectPlayer: SelectPlayer
+  PlayerSimulation: PlayerSimulation
+  GameSimulation: GameSimulation
+
+
---
+++ javascript/BasketballEnglish.js
@@ -0,0 +1,114 @@
+(function() {
+  var GameSimulation, POINT_TYPES, PlayerSimulation, SelectPlayer, Team, loadFile, model, normalizePointType;
+  loadFile = function(filename) {
+    return JsSlim.loadJsFile(filename + '.js');
+  };
+  loadFile('BbScoutModel');
+  model = this.BbScout.model;
+  Team = (function() {
+    function Team(name) {
+      this.name = name;
+      this.team = new model.Team(this.name);
+      model.teams[this.name] = this.team;
+    }
+    Team.prototype.setNumber = function(number) {
+      this.number = number;
+    };
+    Team.prototype.setFirstName = function(firstName) {
+      this.firstName = firstName;
+    };
+    Team.prototype.setLastName = function(lastName) {
+      this.lastName = lastName;
+    };
+    Team.prototype.execute = function() {
+      var player;
+      player = new model.Player(this.number, this.firstName, this.lastName);
+      return this.team.addPlayer(player);
+    };
+    return Team;
+  })();
+  SelectPlayer = (function() {
+    function SelectPlayer(teamname, number) {
+      var _ref;
+      this.teamname = teamname;
+      this.number = number;
+      this.player = (_ref = model.teams[this.teamname]) != null ? _ref.getPlayer(this.number) : void 0;
+    }
+    SelectPlayer.prototype.playerName = function() {
+      var _ref, _ref2;
+      return (_ref = (_ref2 = this.player) != null ? _ref2.name() : void 0) != null ? _ref : 'unknown';
+    };
+    return SelectPlayer;
+  })();
+  POINT_TYPES = {
+    'free throw': 'Freethrow',
+    'free throws': 'Freethrow',
+    'field goal': 'Fieldgoal',
+    'field goals': 'Fieldgoal',
+    'three pointer': 'Threepointer',
+    'three pointers': 'Threepointer'
+  };
+  normalizePointType = function(pointType) {
+    return POINT_TYPES[pointType];
+  };
+  PlayerSimulation = (function() {
+    var NUMBER;
+    NUMBER = '1';
+    return (function() {
+      function _Class() {
+        this.player = new model.Player(NUMBER);
+      }
+      _Class.prototype.points = function() {
+        return this.player.points;
+      };
+      _Class.prototype.setPoints = function(points) {
+        return this.player.points = parseInt(points);
+      };
+      _Class.prototype.scores = function(pointType) {
+        return this.player.scores(normalizePointType(pointType));
+      };
+      _Class.prototype.misses = function(pointType) {
+        return this.player.misses(normalizePointType(pointType));
+      };
+      _Class.prototype.scored = function(pointType) {
+        return this.player.scored(normalizePointType(pointType));
+      };
+      _Class.prototype.attempted = function(pointType) {
+        return this.player.attempted(normalizePointType(pointType));
+      };
+      return _Class;
+    })();
+  })();
+  GameSimulation = (function() {
+    function GameSimulation(teamNameA, teamNameB) {
+      this.teamNameA = teamNameA;
+      this.teamNameB = teamNameB;
+      this.teamA = this.getTeam(this.teamNameA);
+      this.teamB = this.getTeam(this.teamNameB);
+    }
+    GameSimulation.prototype.score = function() {
+      var _ref, _ref2;
+      return ((_ref = this.teamA) != null ? _ref.points() : void 0) + ':' + ((_ref2 = this.teamB) != null ? _ref2.points() : void 0);
+    };
+    GameSimulation.prototype.playerOfScoresA = function(number, teamName, pointType) {
+      var player, pointTypeOk;
+      player = this.getPlayer(teamName, number);
+      pointTypeOk = player != null ? player.scores(normalizePointType(pointType)) : void 0;
+      return (player != null) && pointTypeOk;
+    };
+    GameSimulation.prototype.getPlayer = function(teamName, number) {
+      var _ref;
+      return (_ref = this.getTeam(teamName)) != null ? _ref.getPlayer(number) : void 0;
+    };
+    GameSimulation.prototype.getTeam = function(teamName) {
+      return model.teams[teamName];
+    };
+    return GameSimulation;
+  })();
+  this.BasketballEnglish = {
+    Team: Team,
+    SelectPlayer: SelectPlayer,
+    PlayerSimulation: PlayerSimulation,
+    GameSimulation: GameSimulation
+  };
+}).call(this);
